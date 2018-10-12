5f1f0e65d90dd47a083d8d3078ac8c4c2aaf576d
Christo Goosen <christo.goosen@takectrl.co.za>
2018-10-12 10:47:46 +0200

    Changes for pycon

--- loadtest/example_api.py
+++ loadtest/example_api.py
@@ -1,19 +1,46 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic import response
+import asyncpg
 import uuid
-
+import json
+import time
 app = Sanic()
+# pool = await asyncpg.create_pool(user='goose',
+#                                  database='pagila', host='127.0.0.1')
+# con = await pool.acquire()
 # request_count = 0
 
 @app.route("/<req_uuid>")
 async def test(request, req_uuid):
     request_count =+ 1
-    return json({"hello": "world", "uuid": str(req_uuid)})
+    return response.json({"hello": "world", "uuid": str(req_uuid)})
 
 @app.route("/")
-async def test(request):
+async def root(request):
     request_count =+ 1
-    return json({"hello": "world", "uuid": str(uuid.uuid4())})
+    return response.json({"hello": "world", "uuid": str(uuid.uuid4())})
+
+@app.route("complaints/")
+@app.route("/complaints/<state>")
+async def complaints(request, state):
+    conn = await asyncpg.connect(user='goose',
+                                 database='consumer_complaints', host='127.0.0.1')
+    try:
+        start = time.time()
+        query = '''SELECT index,state,company,product FROM complaints WHERE state LIKE $1 LIMIT 5;'''
+        result = await conn.fetch(query, state)
+        await conn.close()
+        return response.json({
+            "data": result,
+            "query_received": state,
+            "query_time": "% seconds" % (time.time() - start)
+        })
+    except Exception as e:
+
+        return response.json({
+            "error": e
+        })
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, workers=1)
+    app.run(host="127.0.0.1", port=8000, workers=1)
--- loadtest/loadtest.py
+++ loadtest/loadtest.py
@@ -4,19 +4,32 @@ import multiprocessing
 import asyncio
 import time
 import uuid
+import random
+import string
+from itertools import chain
 
 async def fetch(url):
     # return await request("GET", url)
 
     async with aiohttp.ClientSession() as session:
-        async with session.get("{url}".format(url=url)) as resp:
-            # async with session.get("{url}/{uuid}".format(url=url, uuid=uuid.uuid4())) as resp:
-            # print(resp.status)
+        async with session.get(url) as resp:
             return (resp.status, await resp.json())
 
+async def id_generator(size=2, chars=string.ascii_uppercase):
+    ...
+    return ''.join(random.choice(chars) for _ in range(size))
+
 async def run_aiomultiprocess(req_num=10):
+
     # urls = ["http://localhost:8000/", ...]
-    urls = ["http://localhost:8000"]*req_num
+    urls_root = ["http://localhost:8000"]*int(req_num/2)
+    # urls_query = [ 'http://localhost:8000/complaints/{}'.format(
+    #     await id_generator()) for x in range(0,int(req_num/2)
+    #                                          )
+    # ]
+    urls_query = list()
+    urls = list(chain(urls_root, urls_query))
+
     start = time.time()
     async with Pool(processes=(multiprocessing.cpu_count()-1)) as pool:
         results = await pool.map(fetch, urls)
@@ -27,11 +40,18 @@ async def run_aiomultiprocess(req_num=10):
     total_time = end - start
     # req_per_sec = total_time/60
     print("time = ", total_time)
+    print(len(list(results))/total_time, "req/s")
+
 
 async def run_async_only(req_num=10):
-    urls = ["http://localhost:8000", ...]
+    urls_root = ["http://localhost:8000"]*int((req_num / 2))
+    # urls_query = ['http://localhost:8000/complaints/{}'.format(
+    #     await id_generator()) for x in range(0, int(req_num /2))]
+    urls_query = list()
+    urls = list(chain(urls_root, urls_query))
+
     # tasks = [fetch(urls[0])]*req_num
-    tasks = [fetch(urls[0]) for x in range(0,req_num)]
+    tasks = [fetch(x) for x in urls]
     start = time.time()
     results = asyncio.gather(*tasks)
     results = await results
@@ -41,11 +61,12 @@ async def run_async_only(req_num=10):
     # req_per_sec = total_time/60
     print("Num of results = ", len(list(results)))
     print("time = ", total_time)
+    print(len(list(results))/total_time, "req/s")
 
 # if __name__ == "__main__":
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_async_only(req_num=5000))
-    loop.run_until_complete(run_aiomultiprocess(req_num=5000))
+    loop.run_until_complete(run_async_only(req_num=100))
+    loop.run_until_complete(run_aiomultiprocess(req_num=100))
     loop.close()
